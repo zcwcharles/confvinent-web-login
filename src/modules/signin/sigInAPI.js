f14e8d3ed55226b9e53731ebf6da635130fa131f
Charles Wang <zichen.wang.charles@gmail.com>
2023-04-06 21:11:34 -0400

    Update signin api

--- src/modules/signin/sigInAPI.js
+++ src/modules/signin/sigInAPI.js
@@ -1,6 +1,6 @@
 import { post } from '../../requests';
 
 export const signInPost = async body => {
-  const resp = await post('http://confvinent.com:5000/api/auth/signin', body);
+  const resp = await post('/api/auth/signin', body);
   return resp;
 };
