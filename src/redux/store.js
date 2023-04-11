883651b1fbd13eba44388adc9b91b2d58a064311
Charles Wang <zichen.wang.charles@gmail.com>
2023-04-10 20:01:15 -0400

    Update signin code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../components/counter/counterSlice';
 import signinReducer from '../modules/signin/signInSlice';
 import signupReducer from '../modules/signup/signUpSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     signin: signinReducer,
     signup: signupReducer,
   },
